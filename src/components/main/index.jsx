baefb0b0476d09f07521f96dfa1a05057bac62f2
Daehyun Jung <daehyunj1997@gmail.com>
2024-07-12 11:47:27 -0700

    Add a header to the main page

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,10 +1,19 @@
+import { Flex, Heading } from "@chakra-ui/react";
 
-
-
-
-export default function Main(){
-
-    return <div>
-        Main
-    </div>
+export default function Main() {
+  return (
+    <Flex textAlign={"center"} flexDirection={"column"} pr={"5"} pl={"5"}>
+      <Flex alignItems={"center"} justifyContent={"space-between"} mt={"12"}>
+        <Heading
+          color={"blue.400"}
+          display={["none", "block", "block", "block"]}
+        >
+          Expense Tracker
+        </Heading>
+        <Flex>
+            
+        </Flex>
+      </Flex>
+    </Flex>
+  );
 }
